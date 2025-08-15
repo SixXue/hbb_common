7dab10c6fb1caa5f5f05ac8c4f63632e13a089b0
SixXue <six.xue@outlook.com>
2025-08-15 19:34:01 +0800

    改为wlxan1的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["116.131.41.237"];
+pub const RS_PUB_KEY: &str = "Yx9hsAm2rU4qObGucXrjxUh7Qxyn0BLSSQgwlazqht1=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
